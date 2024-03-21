11c9611100a6e5b75a013f04344761f39479b306
stefnotch <brandmairstefan@gmail.com>
2024-03-21 13:40:36 +0100

    Add utils

---
+++ src/map-utils.ts
@@ -0,0 +1,11 @@
+export default {
+  getOrInsert<K, V>(map: Map<K, V>, key: K, defaultValue: () => V): V {
+    if (map.has(key)) {
+      return map.get(key) as V;
+    } else {
+      const value = defaultValue();
+      map.set(key, value);
+      return value;
+    }
+  },
+};
